056d86e0c6a5b24599879b69e060cf0b69411fdd
Daniel Ramirez <daniel@akosweb.com>
2021-03-25 12:54:18 -0700

    Ping the API too

--- heroku.js
+++ heroku.js
@@ -3,10 +3,13 @@ const fetch = require('node-fetch');
 /**
  * Send a request to the Heroku dyno before it sleeps.
  */
- if (process.env.HEROKU_URL) {
+ if (process.env.HEROKU_URL && process.env.API_URL) {
   const pingDyno = () => {
     fetch(process.env.HEROKU_URL).then(() => {
-      console.log('--- WAKE UP ---');
+      console.log('--- WAKE UP BOT ---');
+    });
+    fetch(process.env.API_URL).then(() => {
+      console.log('--- WAKE UP API ---');
     });
   };
   setInterval(pingDyno, 1200000); // Ping every 20 minutes.
